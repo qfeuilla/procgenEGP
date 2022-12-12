95c18144e13dfe6b14fa3beeab596cdf203a4862
research <research@Zorro>
2022-12-12 08:42:10 +0100

    add hardcoded path

--- procgen/src/games/coinrun.cpp
+++ procgen/src/games/coinrun.cpp
@@ -83,7 +83,43 @@ class CoinRun : public BasicAbstractGame {
 
         out_of_bounds_object = WALL_MID;
 
+        /*
         std::string path = global_resource_root + "kenney/Items/";
+        */
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagGreen_down.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/keyYellow.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagGreen2.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagRed2.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagBlue_down.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagYellow_down.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/gemBlue.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagYellow1.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/coinGold.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagYellow2.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/red_line_diag.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/yellow_line_diag.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/coinSilver.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/keyRed.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagRed_down.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagRed1.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagBlue2.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/keyGreen.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/gemRed.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/coinBronze.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/keyBlue.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/gemGreen.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/star.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/gemYellow.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagGreen1.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items/flagBlue1.png");
+
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items_test/bomb.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items_test/saw.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items_test/torch1.png");
+        items.push_back("/home/research/Desktop/procgenEGP/procgen/data/assets/kenney/Items_test/cactus.png");
+
+
+        /*
         for (const auto &entry : std::experimental::filesystem::directory_iterator(path)) {
             items.push_back(entry.path().string().substr(global_resource_root.size()));
         }
@@ -91,6 +127,7 @@ class CoinRun : public BasicAbstractGame {
         for (const auto &entry : std::experimental::filesystem::directory_iterator(path)) {
             items_test.push_back(entry.path().string().substr(global_resource_root.size()));
         }
+        */
     }
 
     void load_background_images() override {
